d5e6bb9acf39ed43956fcff7bbed488050ca81b6
Loris Caputo <46825967+loris1712@users.noreply.github.com>
2025-07-09 16:28:38 +0200

    Changes

--- src/controllers/maintenanceController.js
+++ src/controllers/maintenanceController.js
@@ -229,7 +229,7 @@ exports.updateStatus = async (req, res) => {
     const jobExecutionId = req.params.id;
     const { status_id } = req.body;
 
-    if (!status_id || ![1, 2, 3].includes(Number(status_id))) {
+    if (!status_id || ![1, 2, 3, 4, 5, 6].includes(Number(status_id))) {
       return res.status(400).json({ error: "Invalid or missing status_id. Allowed values: 1 (Attivo), 2 (In pausa), 3 (Non attivo)" });
     }
 
@@ -238,8 +238,13 @@ exports.updateStatus = async (req, res) => {
     if (!jobExecution) {
       return res.status(404).json({ error: "JobExecution not found" });
     }
-
+ 
     jobExecution.status_id = status_id;
+
+    if(status_id == 2){
+      jobExecution.pauseDate = Date.now();
+    }
+ 
     await jobExecution.save();
 
     res.status(200).json({ message: "Status updated successfully", jobExecution });
