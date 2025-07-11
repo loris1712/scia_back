d58d3b5e4ff961449bb02429eecea646970de1a3
Loris Caputo <46825967+loris1712@users.noreply.github.com>
2025-07-11 11:12:14 +0200

    Changes

--- src/routes/maintenanceRoutes.js
+++ src/routes/maintenanceRoutes.js
@@ -5,8 +5,8 @@ const maintenanceController = require("../controllers/maintenanceController");
 router.get("/type", maintenanceController.getTypes);
 router.get("/jobs", maintenanceController.getJobs);
 router.get("/job", maintenanceController.getJob);
-router.patch('/updateStatus/:id', maintenanceController.updateStatus);
-router.patch('/saveStatusComment/:id', maintenanceController.saveStatusComment);
+router.post('/updateStatus/:id', maintenanceController.updateStatus);
+router.post('/saveStatusComment/:id', maintenanceController.saveStatusComment);
 router.patch('/reportAnomaly/:id', maintenanceController.reportAnomaly);
 router.patch('/markAsOk/:id', maintenanceController.markAsOk);
 
